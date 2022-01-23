360d80cd733900a12e2c25ed929751512557bb05
Diwanga Amasith <diwangaa@Direct.local>
2022-01-24 02:07:21 +0530

    Navigation Added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,11 +14,11 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   constructor(private heroService: HeroService,private messageService : MessageService) {}
 
-  selectedHero? : Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // selectedHero? : Hero;
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
 
   // getHeroes(): void {
