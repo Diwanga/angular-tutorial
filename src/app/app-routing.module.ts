af760d871f28a59bcc407d763e82d03460195b2e
Diwanga Amasith <diwangaa@Direct.local>
2022-01-23 23:31:23 +0530

    ROUTING done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+//The @NgModule metadata initializes the router and starts it listening for browser location changes.
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
-import { HEROES } from  "../mock-heros";
+// import { HEROES } from  "../mock-heros";
+import { HeroService } from '../hero.service';
+import { MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes', // the component's CSS element selector
@@ -9,15 +11,26 @@ import { HEROES } from  "../mock-heros";
 })
 export class HeroesComponent implements OnInit {
 
-  heroes : Hero[] = HEROES;
-  constructor() { }
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService,private messageService : MessageService) {}
 
   selectedHero? : Hero;
-  onSelect(hero: Hero): void{
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-  ngOnInit(): void {
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void { //like useeffect
+    this.getHeroes();
   }
 
 }
